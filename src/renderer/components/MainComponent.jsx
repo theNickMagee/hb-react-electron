82a60cccf12475178b49e1eec8fbb4d1159e7299
Nick Magee <nickmageedev@gmail.com>
2024-07-21 22:06:27 -0500

    p5 sketch inside

--- src/renderer/components/MainComponent.jsx
+++ src/renderer/components/MainComponent.jsx
@@ -1,19 +1,26 @@
+import { ReactP5Wrapper } from '@p5-wrapper/react';
+
+function sketch(p5) {
+  p5.setup = () => p5.createCanvas(600, 400, p5.WEBGL);
+
+  p5.draw = () => {
+    p5.background(250);
+    p5.normalMaterial();
+    p5.push();
+    p5.rotateZ(p5.frameCount * 0.01);
+    p5.rotateX(p5.frameCount * 0.01);
+    p5.rotateY(p5.frameCount * 0.01);
+    p5.plane(100);
+    p5.pop();
+  };
+}
+
 const MainComponent = () => {
   return (
     <div className="main-component">
-      {Array.from({ length: 8 }, (_, rowIndex) => (
-        <div key={rowIndex} className="grid-row">
-          {Array.from({ length: 8 }, (_, colIndex) => (
-            <GridSpace key={colIndex} />
-          ))}
-        </div>
-      ))}
+      <ReactP5Wrapper sketch={sketch} />
     </div>
   );
 };
 
 export default MainComponent;
-
-const GridSpace = () => {
-  return <div className="grid-space"></div>;
-};
