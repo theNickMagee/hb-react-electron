c465af704cfa39816f3be05d63996465030af0c1
Nick Magee <nickmageedev@gmail.com>
2024-08-04 03:48:42 -0500

    note improvement

--- src/renderer/components/options/boardObjectOptions/PianoRoll.jsx
+++ src/renderer/components/options/boardObjectOptions/PianoRoll.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import './styles/pianoRoll.css';
 import {
   checkIfEventInNote,
   createEventsFromClick,
@@ -5,110 +7,79 @@ import {
   returnTimePerBeat,
   returnTimePerMeasure,
 } from '../../../services/MidiServices';
-import './styles/pianoRoll.css';
-import React, { useEffect } from 'react';
 
-const PianoRoll = ({ value, setValue, bpm }) => {
-  return (
-    <div className="midi-options">
-      <div className="top-midi-options">
-        <div className="default-dd">
-          Beats per measure
-          <select
-            value={value.timeSignatureTop}
-            onChange={(e) => {
-              const newValue = {
-                ...value,
-                timeSignatureTop: parseInt(e.target.value),
-              };
-              setValue(newValue);
-            }}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value={5}>5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-          </select>
-        </div>
-        <div className="default-dd">
-          Octave
-          <select
-            value={value.octave}
-            onChange={(e) => {
-              const newValue = {
-                ...value,
-                octave: parseInt(e.target.value),
-              };
-              setValue(newValue);
-            }}
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-          </select>
-          {/* move up an octave */}
-          <div
-            className="default-button"
-            onClick={() => {
-              const newValue = {
-                ...value,
-                octave: value.octave > 6 ? 7 : value.octave + 1,
-              };
-              setValue(newValue);
-            }}
-          >
-            +
-          </div>
-          {/* move down an octave */}
-          <div
-            className="default-button"
-            onClick={() => {
-              const newValue = {
-                ...value,
-                octave: value.octave < 2 ? 1 : value.octave - 1,
-              };
-              setValue(newValue);
-            }}
-          >
-            -
-          </div>
-        </div>
+const PianoRoll = ({ value, setValue, bpm }) => (
+  <div className="midi-options">
+    <div className="top-midi-options">
+      <div className="default-dd">
+        Beats per measure
+        <select
+          value={value.timeSignatureTop}
+          onChange={(e) =>
+            setValue({
+              ...value,
+              timeSignatureTop: parseInt(e.target.value, 10),
+            })
+          }
+        >
+          {[...Array(7).keys()].map((n) => (
+            <option key={n} value={n + 1}>
+              {n + 1}
+            </option>
+          ))}
+        </select>
       </div>
-      <div className="middle-midi-section">
-        <div className="piano-roll">
-          <PianoGrid
-            events={value.events}
-            setEvents={(events) => {
-              const newValue = { ...value, events };
-              setValue(newValue);
-            }}
-            octave={value.octave}
-            numMeasures={value.timeSignatureTop}
-            bpm={bpm}
-          />
-        </div>
-      </div>
-      <div className="bottom-midi-options">
-        {/* Time Signature: {value.timeSignatureTop}/4 */}
-        <div className="small-font">
-          Time per beat: {returnTimePerBeat(bpm)}s
-        </div>
-        <div className="small-font">
-          Time per measure: {returnTimePerMeasure(bpm, value.timeSignatureTop)}s
-        </div>
+      <div className="default-dd">
+        Octave
+        <select
+          value={value.octave}
+          onChange={(e) =>
+            setValue({ ...value, octave: parseInt(e.target.value, 10) })
+          }
+        >
+          {[...Array(7).keys()].map((n) => (
+            <option key={n} value={n + 1}>
+              {n + 1}
+            </option>
+          ))}
+        </select>
+        <button
+          className="default-button"
+          onClick={() =>
+            setValue({ ...value, octave: Math.min(value.octave + 1, 7) })
+          }
+        >
+          +
+        </button>
+        <button
+          className="default-button"
+          onClick={() =>
+            setValue({ ...value, octave: Math.max(value.octave - 1, 1) })
+          }
+        >
+          -
+        </button>
       </div>
     </div>
-  );
-};
-
-export default PianoRoll;
+    <div className="middle-midi-section">
+      <div className="piano-roll">
+        <PianoGrid
+          events={value.events}
+          setEvents={(newValue) => setValue({ ...value, events: newValue })}
+          octave={value.octave}
+          numMeasures={value.timeSignatureTop}
+          bpm={bpm}
+        />
+      </div>
+    </div>
+    <div className="bottom-midi-options">
+      <div className="small-font">Time per beat: {returnTimePerBeat(bpm)}s</div>
+      <div className="small-font">
+        Time per measure: {returnTimePerMeasure(bpm, value.timeSignatureTop)}s
+      </div>
+    </div>
+  </div>
+);
 
 const PianoGrid = ({ events, setEvents, octave, numMeasures, bpm }) => {
   const octaveNotes = [
@@ -126,103 +97,77 @@ const PianoGrid = ({ events, setEvents, octave, numMeasures, bpm }) => {
     'B',
   ];
 
-  const isWhiteKey = (note) => {
-    return !note.includes('#');
-  };
-
   return (
-    <>
-      <div className="piano-grid">
-        {/* absolute positioned notes with length */}
-        {/*  for every event pair */}
-        {octaveNotes.map((note, idx) => (
-          <div className="piano-row" key={idx}>
-            <div className="note-label">{note}</div>
-            <EventNotes
-              events={events}
-              octave={octave}
-              numMeasures={numMeasures}
-              note={note}
-              bpm={bpm}
-            />
-            <div
-              key={idx}
-              className="piano-key"
-              style={{
-                backgroundColor: isWhiteKey(note) ? '#221' : '#111',
-              }}
-            >
-              {/* for every measure */}
-              {Array.from({ length: numMeasures }, (_, measure) => (
-                <div
-                  key={measure}
-                  className={`measure ${
-                    checkIfEventInNote(
-                      events,
-                      octave,
-                      note,
-                      measure,
-                      bpm,
-                      numMeasures,
-                    ) && 'active'
-                  }`}
-                  onClick={() =>
-                    createEventsFromClick(
-                      events,
-                      setEvents,
-                      note,
-                      octave,
-                      measure,
-                      bpm,
-                      numMeasures,
-                    )
-                  }
-                >
-                  {/* {checkIfEventInNote(
-                  events,
-                  octave,
-                  note,
-                  measure,
-                  bpm,
-                  numMeasures,
-                ) && 'x'} */}
-                </div>
-              ))}
-            </div>
+    <div className="piano-grid">
+      {octaveNotes.map((note, idx) => (
+        <div className="piano-row" key={note}>
+          <div className="note-label">{note}</div>
+          <EventNotes
+            events={events}
+            octave={octave}
+            numMeasures={numMeasures}
+            note={note}
+            bpm={bpm}
+          />
+          <div
+            className="piano-key"
+            style={{ backgroundColor: note.includes('#') ? '#111' : '#221' }}
+          >
+            {Array.from({ length: numMeasures }).map((_, measure) => (
+              <div
+                key={measure}
+                className={`measure ${checkIfEventInNote(events, octave, note, measure, bpm, numMeasures) ? 'active' : ''}`}
+                onClick={() =>
+                  createEventsFromClick(
+                    events,
+                    setEvents,
+                    note,
+                    octave,
+                    measure,
+                    bpm,
+                    numMeasures,
+                  )
+                }
+              />
+            ))}
           </div>
-        ))}
-      </div>
-    </>
+        </div>
+      ))}
+    </div>
   );
 };
 
 const EventNotes = ({ events, octave, numMeasures, note, bpm }) => {
-  // display events on current note absolute positions with margin left correct ontop of row
-
-  const noteEvents = events.filter((event) => event.note.includes(note));
+  const noteEvents = findPairsInNoteAndOctave(
+    bpm,
+    numMeasures,
+    events,
+    note,
+    octave,
+  );
   const timePerEntireLine =
     returnTimePerMeasure(bpm, numMeasures) * numMeasures;
 
-  return findPairsInNoteAndOctave(bpm, numMeasures, events, note, octave).map(
-    ({ noteOn, noteOff, measure }) => {
-      const noteLength = noteOff.time - noteOn.time;
-      const notePosition = (noteOn.time / timePerEntireLine) * 100;
-      const noteWidth = (noteLength / timePerEntireLine) * 100;
+  return noteEvents.map(({ noteOn, noteOff }, index) => {
+    const noteLength = noteOff.time - noteOn.time;
+    const notePosition = (noteOn.time / timePerEntireLine) * 100;
+    const noteWidth = (noteLength / timePerEntireLine) * 100;
 
-      return (
-        <div
-          key={noteOn.time}
-          className="event-note"
-          style={{
-            left: `0`,
-            marginLeft: `${notePosition}%`,
-            width: `${noteWidth}%`,
-            position: 'absolute',
-            backgroundColor: 'rgba(255, 3, 255, 0.5)',
-            height: '100%',
-          }}
-        ></div>
-      );
-    },
-  );
+    return (
+      <div
+        key={index}
+        className="event-note"
+        style={{
+          left: `1.5rem`,
+          marginLeft: `${notePosition}%`,
+          width: `${noteWidth}%`,
+          position: 'absolute',
+          backgroundColor: 'rgba(255, 3, 255, 0.5)',
+          height: '100%',
+        }}
+      ></div>
+    );
+  });
 };
+
+export default PianoRoll;
