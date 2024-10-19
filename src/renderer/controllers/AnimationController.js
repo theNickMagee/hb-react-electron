1301afc27078989b3b8a2002df85da1e1d41849a
Nick Magee <nickmageedev@gmail.com>
2024-10-19 04:49:43 -0500

    sorting animation data

--- src/renderer/controllers/AnimationController.js
+++ src/renderer/controllers/AnimationController.js
@@ -1,26 +1,76 @@
 import { medusaAnimationCoords } from '../defaults/HeroAnimationDefaults';
 import { gladiatorAnimationCoords } from '../defaults/HeroAnimationDefaults';
+import { getMasterTimeOfEvent } from '../services/HeroServices';
 
 class HeroStateChange {
-  constructor(heroId, time, newState, newFrame) {
+  constructor(heroId, time, newState, newFrame, targetBoardObjectId) {
     this.heroId = heroId;
     this.time = time;
     this.newState = newState;
     this.newFrame = newFrame;
+    this.targetBoardObjectId = targetBoardObjectId;
   }
 }
 
 const runHeroAnimations = (heroEvents, bpm, data, setData) => {
   // loop thru events and create prpoper state changes
-
+  console.log('runHeroAnimations: ', heroEvents, bpm, data);
   // heroEvents is an odd object
-  // [{events: [{heroId, targetBoardObjectId, measure}], path: []
+  // {paths: [events: [{heroId, targetBoardObjectId, measure}]]}
 
-  let stateChanges = [];
+  // we wil use only the events array
 
-  console.log('runHeroAnimations: ', heroEvents);
+  let stateChanges = createHeroStateChanges(heroEvents, bpm);
+
+  console.log('runHeroAnimations stateChanges: ', stateChanges);
 
   // will need to find hero and set options[0] currentFrame and currentState accordingly
+};
+
+const createHeroStateChanges = (heroEvents, bpm) => {
+  let allEvents = [];
+
+  for (let i = 0; i < heroEvents.paths.length; i++) {
+    for (let j = 0; j < heroEvents.paths[i].events.length; j++) {
+      const currentEvent = heroEvents.paths[i].events[j];
+      const masterEventStartTime = getMasterTimeOfEvent(currentEvent, bpm);
+
+      // create a new state change
+
+      allEvents.push({
+        heroId: currentEvent.heroId,
+        time: masterEventStartTime,
+        event: currentEvent,
+      });
+    }
+  }
+
+  // create double nested list of seperated by unique heroId
+  let stateChanges = [];
+  let heroIds = allEvents.map((event) => event.heroId);
+  heroIds = [...new Set(heroIds)];
+
+  for (let i = 0; i < heroIds.length; i++) {
+    stateChanges.push([]);
+  }
+
+  for (let i = 0; i < allEvents.length; i++) {
+    for (let j = 0; j < heroIds.length; j++) {
+      if (allEvents[i].heroId === heroIds[j]) {
+        stateChanges[j].push(
+          new HeroStateChange(
+            allEvents[i].heroId,
+            allEvents[i].time,
+            allEvents[i].event.action,
+            allEvents[i].event.targetValue,
+            allEvents[i].event.targetBoardObjectId,
+          ),
+        );
+      }
+    }
+  }
+
+  return stateChanges;
 };
 
 const getCoordsFromFrameAndState = (heroName, state, frame) => {
